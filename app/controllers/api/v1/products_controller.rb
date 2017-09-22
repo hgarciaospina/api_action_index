f1670af4ef3268ffbeb6c4e82a3f6e596e079da7
henrygarciaospina <henrygarcia0309@gmail.com>
2017-09-22 18:12:53 -0500

    Error en el método create de la API, no reconoce el método product_params

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     @products = Product.all
   end
+
+  def create
+      @product = Product.new(product_params)
+
+      if @product.save
+        render json: @product, status: :created, location: @product
+      else
+        render json: @product.errors, status: :unprocessable_entity
+      end
+  end
+
+private
+ # Use callbacks to share common setup or constraints between actions.
+  def product_params
+      params.require(:product).permit(:name, :price)
+  end
+
 end
